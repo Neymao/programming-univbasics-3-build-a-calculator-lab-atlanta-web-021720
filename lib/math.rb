636dba183454a9b5eb2b8bc55bdfc50f79d6fb50
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-14 00:11:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
+  
+  1 + 2
 
 end
 
 def subtraction(num1, num2)
+  
+  2 - 1
 
 end
 
 def division(num1, num2)
+  
+4 / 2
 
 end
 
 def multiplication(num1, num2)
 
+2 * 3
+
 end
 
 def modulo(num1, num2)
 
+9 / 4
 end
 
 def square_root(num)
+  
+  5
 
 end
